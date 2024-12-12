fc3d16ed31d6c3c1417584177eb7c73de96cd752
StanislavDevel <unfeel00@gmail.com>
2024-12-12 15:01:14 +0100

    refactor(extended-2.1): replace  classes html/css  BEM

--- app.js
+++ app.js
@@ -9,8 +9,8 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var addButton=document.querySelector(".tasks__button");//first button
+var incompleteTaskHolder=document.querySelector(".todo-list__tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
